bb96b96ab4dd1458ac6db8310830f30e7e391f2d
Wiwi <wiwi880331@gmail.com>
2021-06-15 00:02:32 +0800

    Initial Commit

---
+++ HamsterCard/HamsterCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  HamsterCardApp.swift
+//  HamsterCard
+//
+//  Created by wiwi on 2021/6/15.
+//
+
+import SwiftUI
+
+@main
+struct HamsterCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
